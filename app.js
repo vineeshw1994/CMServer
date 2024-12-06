8e8d79b252b84defcd417429b8704edf55a01bfb
RAHITH KR <rahith@entegation.com>
2024-12-06 10:44:33 +0530

    changes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(bodyParser.json()); // For parsing application/json
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176'],
   credentials: true,
   // exposedHeaders: ["set-cookie"],
 }));
